6e9435094800f7ffb67f4ed1330c20f5fb29eda1
Anuj poudel <anujpoudel54@gmail.com>
2026-05-05 23:15:10 +0545

    refactor GUI as OOP

--- game.py
+++ game.py
@@ -4,137 +4,156 @@ import game_configs as configs
 # 1. Initialize Pygame
 pygame.init()
 
-# 2. Setup constants and window
-WIDTH, HEIGHT = configs.WINDOW_WIDTH, configs.WINDOW_HEIGHT
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-screen_rect = SCREEN.get_rect()
-pygame.display.set_caption(configs.TITLE_CAPTION)
-
-# 3. Setup clock for frame rate control
-clock = pygame.time.Clock()
-FPS = 60
-
-winning_lists = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [3,4,6],
-    [6,7,8],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-]
-
-font = pygame.font.SysFont(None, configs.CHAR_SIZE)
-info_font = pygame.font.SysFont(None, configs.INFO_TEXT_SIZE)
-
-rects: list[pygame.rect.Rect] = []
-
-info_surface = pygame.surface.Surface((WIDTH, HEIGHT * .2), 0, SCREEN)
-info_surface_rect = info_surface.get_rect(topleft=(0,0))
-
-game_surface = pygame.surface.Surface((WIDTH, HEIGHT * .8), 0, SCREEN)
-game_surface_rect = game_surface.get_rect(topleft=(0, info_surface.get_height()))
-
-platform = game_surface
-pl_width = platform.get_width()
-pl_height = platform.get_height()
-
-BOX_WIDTH = (pl_width) // 3
-BOX_HEIGHT = (pl_height) // 3
-
-def check_game_won(board: list):
-    global winning_lists
-
-    for wl in winning_lists:
-        char = board[wl[0]]
-        if char == "0":
-            continue
-        won = True
-        for ind in wl[1:]:
-            won = won and char == board[ind]
-            char = board[ind]
-        if won:
-            return True, char, wl
-    return False, "", []
-
-
-def main():
-    # board = "XXXOXOXOO"
-    board = "0"*9
-    # board = "123456789"
-    board = list(board)
-
-    assert len(board) == 9, "Invalid board"
+class TicTacToe:
     
-    turn = "X"
-    running = True
-    won = False
-    char = "0"
-    won_indexs = []
-    while running:
-        # 4. Event handling loop
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                for ind, rect in enumerate(rects):
-                    rect.y += game_surface_rect.y
-                    if rect.collidepoint(mouse_pos) and board[ind] == "0" and not won:
-                        board[ind] = turn
-                        turn = "X" if turn == "O" else "O"
-                        won, char, won_indexs = check_game_won(board)
+    def __init__(self) -> None:
+        pygame.init()
+        self.board = self.init_board()
+        self._running = True
+        self.turn = "X"
+        self.won = False
+        self.won_char = ""
+        self.won_indexs = []
 
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN and won:
-                board = list("0"*9)
-                won = False
+        # 2. Setup constants and window
+        self.WIDTH, self.HEIGHT = configs.WINDOW_WIDTH, configs.WINDOW_HEIGHT
+        self.SCREEN = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.screen_rect = self.SCREEN.get_rect()
+        pygame.display.set_caption(configs.TITLE_CAPTION)
 
-        # 6. Drawing / Rendering
-        SCREEN.fill(configs.BLACK)  # Clear screen with black
-        SCREEN.blit(info_surface, (0, 0))
-        SCREEN.blit(game_surface, game_surface_rect)
-        info_surface.fill(configs.OFFWHITE)
+        # 3. Setup clock for frame rate control
+        self.clock = pygame.time.Clock()
+        self.FPS = 60
 
-        turn_text = info_font.render(f"Turn: {turn}", 1, configs.BLACK)
-        turn_text_rect = turn_text.get_rect(center=info_surface_rect.center)
+        self.winning_lists = [
+            [0,1,2],
+            [0,3,6],
+            [0,4,8],
+            [1,4,7],
+            [3,4,6],
+            [6,7,8],
+            [2,5,8],
+            [2,4,6],
+            [3,4,5],
+        ]
 
-        info_surface.blit(turn_text, turn_text_rect)
-        game_surface.fill(configs.GRAY)
+        self.font = pygame.font.SysFont(None, configs.CHAR_SIZE)
+        self.info_font = pygame.font.SysFont(None, configs.INFO_TEXT_SIZE)
 
-        if won:
-            st_ind = won_indexs[0]
-            end_ind = won_indexs[2]
-            st_rect, end_rect = rects[st_ind], rects[end_ind]
-            pygame.draw.line(game_surface, configs.GREEN, st_rect.center, end_rect.center, 5)
+        self.rects: list[pygame.rect.Rect] = []
 
+        self.info_surface = pygame.surface.Surface((self.WIDTH, self.HEIGHT * .2), 0, self.SCREEN)
+        self.info_surface_rect = self.info_surface.get_rect(topleft=(0,0))
 
-        rects.clear()
+        self.game_surface = pygame.surface.Surface((self.WIDTH, self.HEIGHT * .8), 0, self.SCREEN)
+        self.game_surface_rect = self.game_surface.get_rect(topleft=(0, self.info_surface.get_height()))
+
+        self.platform = self.game_surface
+        self.pl_width = self.platform.get_width()
+        self.pl_height = self.platform.get_height()
+
+        self.BOX_WIDTH = (self.pl_width) // 3
+        self.BOX_HEIGHT = (self.pl_height) // 3
+
+    def start_game_loop(self):
+        while self._running:
+            self.update()
+
+        pygame.quit()
+
+    def set_char(self, ind, char):
+        self.board[ind] = char
+
+    def _render_board(self):
         char_ind = 0
         for i in range(3):
-            x = (i * BOX_WIDTH)
+            x = (i * self.BOX_WIDTH)
             for j in range(3):
-                y = (j * BOX_HEIGHT)
-                rect_value = (x, y, BOX_WIDTH, BOX_HEIGHT)
-                rect = pygame.draw.rect(platform, configs.OFFWHITE, rect_value, 1)
-                rects.append(rect)
-                char = board[char_ind]
+                y = (j * self.BOX_HEIGHT)
+                rect_value = (x, y, self.BOX_WIDTH, self.BOX_HEIGHT)
+                rect = pygame.draw.rect(self.platform, configs.OFFWHITE, rect_value, 1)
+                self.rects.append(rect)
+                char = self.board[char_ind]
                 if char == "0":
                     char_ind += 1
                     continue
 
-                text_surf = font.render(char, True, configs.BLACK)
-                text_rect = text_surf.get_rect(center=rect.center)
-                game_surface.blit(text_surf, text_rect)
+                text_surf, text_rect = self.create_text(char, configs.BLACK, None, center=rect.center)
+                self.game_surface.blit(text_surf, text_rect)
                 char_ind += 1
+
+    def update(self):
+        self.handle_events()
+
+        # 6. Drawing / Rendering
+        self.SCREEN.fill(configs.BLACK)  # Clear screen with black
+        self.SCREEN.blit(self.info_surface, (0, 0))
+        self.SCREEN.blit(self.game_surface, self.game_surface_rect)
+        self.info_surface.fill(configs.OFFWHITE)
+
+        turn_text, turn_text_rect = self.create_text(f"Turn: {self.turn}", configs.BLACK, None, center=self.info_surface_rect.center)
+        self.info_surface.blit(turn_text, turn_text_rect)
+        self.game_surface.fill(configs.GRAY)
+
+        if self.won:
+            st_ind = self.won_indexs[0]
+            end_ind = self.won_indexs[2]
+            st_rect, end_rect = self.rects[st_ind], self.rects[end_ind]
+            pygame.draw.line(self.game_surface, configs.GREEN, st_rect.center, end_rect.center, 5)
+
+        self.rects.clear()
+        self._render_board()
 
         pygame.display.flip()  # Update the full display Surface to the screen
 
         # 7. Control the frame rate
-        clock.tick(FPS)
+        self.clock.tick(self.FPS)
 
-    pygame.quit()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                for ind, rect in enumerate(self.rects):
+                    rect.y += self.game_surface_rect.y
+                    if rect.collidepoint(mouse_pos) and self.board[ind] == "0" and not self.won:
+                        self.board[ind] = self.turn
+                        self.turn = "X" if self.turn == "O" else "O"
+                        self.won, self.won_char, self.won_indexs = self.check_game_won()
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN and self.won:
+                self.board = list("0"*9)
+                self.won = False
+
+
+    def init_board(self):
+        return list("0"*9)
+
+    def check_game_won(self):
+        for wl in self.winning_lists:
+            char = self.board[wl[0]]
+            if char == "0":
+                continue
+            won = True
+            for ind in wl[1:]:
+                won = won and char == self.board[ind]
+                char = self.board[ind]
+            if won:
+                return True, char, wl
+
+        return False, "", []
+
+    def create_text(self, text: str, color, font: pygame.font.Font| None = None, **rect_kwargs):
+        if font is None:
+            font = pygame.font.SysFont(None, configs.CHAR_SIZE)
+
+        turn_text = self.info_font.render(text, 1, color)
+        turn_text_rect = turn_text.get_rect(**rect_kwargs)
+
+        return turn_text, turn_text_rect
 
 if __name__ == "__main__":
-    main()
+    ttt = TicTacToe()
+    ttt.start_game_loop()
