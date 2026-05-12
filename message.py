a2bacf22882507c09949b41ce6994a67042f08e1
Anuj-Poudel54 <anujpoudel54@gmail.com>
2026-05-12 15:34:29 +0545

    create scaffholding for better server and message implementation

--- message.py
+++ message.py
@@ -1,33 +1,52 @@
 from enum import Enum
-from dataclasses import dataclass
 
-class MessageType(Enum):
-    INFO = 0
-    IN_GAME = 1
+class MessageType(Enum): 
+    PLAY        = 0 # for game play related
+    PLAYER_QUIT = 1 # if player quits the game in between
+    SERVER_MSG  = 2 # server sends message to player
+    PLAYER_MSG  = 3 # player sending message to another player
 
-type_initial_map = {
-    MessageType.INFO: "I",
-    MessageType.IN_GAME: "G",
-}
-
-@dataclass
 class Message:
-    msg: str
-    # msg_type: MessageType | None = None
+    def __init__(self, msg: bytes | str, message_type: int | None = None):
+        if isinstance(msg, (bytes, bytearray)):
+            self._msg = str(msg)
+        else:
+            self._msg = msg
 
-    def __post_init__(self):
-        self.determine_message_type()
+        self.type: int | None = message_type if message_type else None
+        self._decode_msg()
 
-    def determine_message_type(self):
+    def _decode_msg(self):
+        raise NotImplementedError()
 
-        self.msg_type = None
+    def build_message_for_socket(self):
+        return f"{self.type} {self.msg}".encode()
 
-        if self.msg.startswith("IG"):
-            self.msg_type = MessageType.IN_GAME
+class ServerMessage(Message):
+    """
+    message architecture from server to client
+    `message_type msg`
+    """
+    def __init__(self, msg: str | bytes):
+        super().__init__(msg)
 
-        elif self.msg.startswith("I"):
-            self.msg_type = MessageType.INFO
+    def _decode_msg(self):
+        msg_type, self.msg = self._msg.split(" ")
+        if msg_type.isnumeric():
+            self.type = int(msg_type)
 
-        if self.msg_type:
-            self.msg = self.msg[len(type_initial_map.get(self.msg_type, "")):]
+class ClientMessage(Message):
+    """
+    message architecture from server to client
+    `room_id message_type msg`
+    """
 
+    def __init__(self, msg: str | bytes):
+        self.room_id: str
+        super().__init__(msg)
+
+    def _decode_msg(self):
+        self.room_id, self.type, self.msg = self._msg.split(" ")
+
+    def build_message_for_socket(self):
+        return f"{self.room_id} {self.type} {self.msg}".encode()
--- server_socket.py
+++ server_socket.py
@@ -1,5 +1,25 @@
 import socket
 from threading import Thread
+from dataclasses import dataclass, field
+import uuid
+from message import Message
+
+
+@dataclass
+class Player:
+    sock: socket.socket
+    id: uuid.UUID = field(default=uuid.uuid4())
+
+
+@dataclass
+class GameManager:
+    player1: Player
+    player2: Player
+    board: list[str]
+
+    def broadcast_msg(self, msg: bytes):
+        self.player1.sock.send(msg)
+        self.player2.sock.send(msg)
 
 class ServerSockert(socket.socket):
 
@@ -10,7 +30,7 @@ class ServerSockert(socket.socket):
         self.bind((self.HOST, self.PORT))
         self.clients: list[socket.socket] = []
         self.waiter: socket.socket | None = None
-        self.game_rooms: dict[str, tuple[socket.socket, socket.socket]] = {}
+        self.game_rooms: dict[str, GameManager] = {}
 
     def _get_server_host(self):
         with open("server_host.txt", "r") as f:
@@ -30,11 +50,10 @@ class ServerSockert(socket.socket):
             if isinstance(msg, str):
                 msg = msg.encode()
 
-            socks = self.game_rooms.get(room_id)
-            if not socks:
+            gm = self.game_rooms.get(room_id)
+            if not gm:
                 return
-            for sock in socks:
-                sock.send(msg)
+            gm.broadcast_msg(msg)
 
     def _read_msg(self, sock: socket.socket):
         msg = sock.recv(1024).decode()
@@ -54,8 +73,10 @@ class ServerSockert(socket.socket):
 
     def _create_room(self, sock1: socket.socket, sock2: socket.socket):
         new_id = str(self._new_room_id())
-        self.game_rooms[new_id] = (sock1, sock2)
-        # self._broadcast_room(new_id, self._build_info_msg("MATCH_FOUND"))
+        self.game_rooms[new_id] = GameManager(
+                                            player1 = Player(sock1),
+                                            player2 = Player(sock2),
+                                            board   = list("0"*9))
 
         Thread(target=self._handle_client_socket, args=(sock1,), daemon=True).start()
         Thread(target=self._handle_client_socket, args=(sock2,), daemon=True).start()
